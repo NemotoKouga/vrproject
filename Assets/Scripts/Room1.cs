66d25cce9decb2b90aa85b1ca26893b3f58bcdfb
NemotoKouga <s20a5088vk@s.chibakoudai.jp>
2023-10-08 14:02:57 +0900

    Room1移動

--- Assets/Scripts/Room1.cs
+++ Assets/Scripts/Room1.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Room1 : MonoBehaviour
 {
+    public void ClickRoom1(){
+        SceneManager.LoadScene("Room1");
+    }
     // Start is called before the first frame update
     void Start()
     {
