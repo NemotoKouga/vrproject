6dafbb8a8a9121c4f02ecc23d11dc6dc6c9a21fa
NemotoKouga <s20a5088vk@s.chibakoudai.jp>
2023-10-17 16:19:36 +0900

    スライド

---
+++ Assets/Scripts/PhotoChange.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PhotoChange : MonoBehaviour
+{
+    private Image img;
+    private int count = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GameObject.Find("Image").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    public void Next()
+    {
+        count++;
+        if(count>=7)
+        SceneManager.LoadScene("SampleScene");
+
+        img.sprite = Resources.Load<Sprite>("Image/photo" + count.ToString());
+    }
+
+    public void Back()
+    {
+        count--;
+        if(count<=0)
+        count=1;
+
+        img.sprite = Resources.Load<Sprite>("Image/photo" + count.ToString());
+    }
+}
